9ebd79062b130435698289582f1f432bd83e229c
Pratik <pratik.tembhurne@ksolves.com>
2022-04-19 13:03:41 +0530

    CarHub LWC Project

---
+++ force-app/main/default/lwc/similarCars/similarCars.js
@@ -0,0 +1,40 @@
+import { api, LightningElement, wire } from 'lwc';
+import getSimilarCars from '@salesforce/apex/CarController.getSimilarCars'
+import { getRecord } from 'lightning/uiRecordApi';
+import MAKE_TYPE from '@salesforce/schema/Car__c.Make__c'
+import {NavigationMixin} from 'lightning/navigation'
+
+export default class SimilarCars extends LightningElement {
+
+    @api recordId
+    @api objectApiName
+    similarCars
+
+    @wire(getRecord, {recordId: '$recordId', fields: [MAKE_TYPE]})
+    car
+
+    fetchSimilarCars(){
+        getSimilarCars({
+            carId: this.recordId,
+            makeType: this.car.data.fields.Make__c.value
+        }).then(result => {
+            this.similarCars= result
+            console.log(this.similarCars)
+        }).catch(error => {
+            console.error(error)
+        })
+
+    }
+
+    handleViewDetailsClick(event){
+        this[NavigationMixin.Navigate]({
+            type:'standard__recordPage',
+            attributes:{
+                recordId: event.target.dataset.Id,
+                objectApiName:this.objectApiName,
+                actionName:'view'
+            }
+        })
+
+    }
+}
